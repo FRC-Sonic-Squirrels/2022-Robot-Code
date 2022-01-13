f72ca4599f693a78832c807a3dcb90cbc5dbabe8
LuminousLlama <keyushattarde@gmail.com>
2022-01-12 17:29:17 -0800

    Hopefully fixed, files should now update

---
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+import java.util.function.DoubleSupplier;
+
+public class DefaultDriveCommand extends CommandBase {
+    private final Drivetrain m_drivetrainSubsystem;
+
+    private final DoubleSupplier m_translationXSupplier;
+    private final DoubleSupplier m_translationYSupplier;
+    private final DoubleSupplier m_rotationSupplier;
+
+    public DefaultDriveCommand(Drivetrain drivetrain,
+                               DoubleSupplier translationXSupplier,
+                               DoubleSupplier translationYSupplier,
+                               DoubleSupplier rotationSupplier) {
+        this.m_drivetrainSubsystem = drivetrain;
+        this.m_translationXSupplier = translationXSupplier;
+        this.m_translationYSupplier = translationYSupplier;
+        this.m_rotationSupplier = rotationSupplier;
+
+        addRequirements(drivetrain);
+    }
+
+
+
+    @Override
+    public void execute() {
+        // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement
+        m_drivetrainSubsystem.drive(
+                ChassisSpeeds.fromFieldRelativeSpeeds(
+                        m_translationXSupplier.getAsDouble(),
+                        m_translationYSupplier.getAsDouble(),
+                        m_rotationSupplier.getAsDouble(),
+                        m_drivetrainSubsystem.getGyroscopeRotation()
+                )
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+    }
+  }
