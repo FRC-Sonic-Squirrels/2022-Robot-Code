dc89ff26d8e98ee76dc4b9ad8223f4ccd2d51938
LuminousLlama <keyushattarde@gmail.com>
2022-01-12 17:24:43 -0800

    Testing commit git is very broken for me.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.Drivetrain;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +16,40 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+  /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = Units.inchesToMeters(19.0);
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(19.0); 
+
+    // Set pigeon ID to -1 to disable and use NAVX on SPI.Port.kMXP
+    public static final int DRIVETRAIN_PIGEON_ID = 15;
+    
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; 
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 11;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 21;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(142.4 + 180);
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 12;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(65.4 + 180);
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 14; 
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 24; 
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(28.1 + 180);
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 3;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 13; 
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(182.7 - 180);
+}
