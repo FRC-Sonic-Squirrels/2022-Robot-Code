4c2b99be786ff397a958c51d7bf0ecc3d6e8e441
Bryn Dole <bryn.dole@gmail.com>
2022-01-22 13:50:52 -0800

    swerve template code w/ simple auton

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,7 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -17,7 +18,7 @@ import frc.robot.subsystems.Drivetrain;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  /**
+    /**
      * The left-to-right distance between the drivetrain wheels
      *
      * Should be measured from center to center.
@@ -52,4 +53,25 @@ public final class Constants {
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 13; 
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23;
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(182.7 - 180);
+
+    public static final class AutoConstants {
+        // This kP worked for the DriveWithSetRotation command
+        public static final double kPThetaController = 3.0;
+        public static final double kIThetaController = 0.0;
+        public static final double kDThetaController = 0.02;
+    
+        // Feed Forward and PID values from SysId
+        public static final double kP = 2.3055;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kA = 0.12817;
+        public static final double kV = 2.3423;
+        public static final double kS = 0.53114;
+
+        // Constraint for the motion profiled robot angle controller
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+                new TrapezoidProfile.Constraints(
+                        DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+                        DrivetrainSubsystem.MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED);
+    }
 }
