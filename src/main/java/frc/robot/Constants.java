e2c8c4737c703f795d9e95f5f5c78e731c8c7b4b
miamanzella <miamanzella@gmail.com>
2022-01-24 19:56:20 -0800

    Initial copy of the Indexer class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,57 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+  // Allocate CAN Ids from here. 
+    // This avoids accidentally assigning the same CAN id to two different devices.
+    public static final class canId {
+      // CAN Id 0 is off limits. Typically unconfigured devices default to CAN id zero. This will 
+      // create problems if you already have a device using CAN id 0 on the CAN bus.
+      public static final int DoNotUse_canId0 = 0;
+
+     // When assigning a CAN ID, rename the constant to something descriptive. Such as
+     // when assigning CAN 1 rename "canId1" to "driveLeftLead" or "pigeonIMU"
+      public static final int canId1 = 1;
+      public static final int canId2 = 2;
+      public static final int canId3 = 3;
+      public static final int canId4_hopper_agitator = 4;
+      public static final int canId5_turret = 5;
+      public static final int canId6_hood = 6;
+      public static final int canId7 = 7;
+      public static final int canId8_indexo_intake_and_hopper = 8;
+      public static final int canId9 = 9;
+      public static final int canId10_indexo_belts = 10;
+      public static final int canId11_indexo_kicker = 11;
+      public static final int canId12_drive_left_lead = 12;
+      public static final int canId13_drive_left_follow = 13;
+      public static final int canId14_drive_right_follow = 14;
+      public static final int canId15_drive_right_lead = 15;
+      public static final int canId16_flywheel_right = 16;
+      public static final int canId17_flywheel_left = 17;
+      public static final int canId18_intake = 18;
+      public static final int canId19 = 19;
+      public static final int canId20_pigeon_imu = 20;
+  }
+
+  public static final class indexConstants {
+    public static final int hopperAgitator = canId.canId4_hopper_agitator;
+    public static final int indexIntake = canId.canId8_indexo_intake_and_hopper;
+    public static final int indexBelts = canId.canId10_indexo_belts;
+    public static final int indexKicker = 11;
+    public static final double agitatorRPM = 500.0;
+  }
+
+  public static final class digitalIOConstants {
+    // assign digital IO (DIO) ports 0-9
+    public static final int dio0_indexerSensor1 = 0;
+    public static final int dio1_indexerSensor2 = 1;
+    public static final int dio2_indexerSensor3 = 2;
+  }
+
+  public static final class currentLimits {
+    public static SupplyCurrentLimitConfiguration m_currentlimitMain = new SupplyCurrentLimitConfiguration(true, 35, 1, 1);
+    public static SupplyCurrentLimitConfiguration m_currentlimitSecondary = new SupplyCurrentLimitConfiguration(true, 25, 1, 1);
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,417 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrame;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.indexConstants;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.RelativeEncoder;
+
+import static frc.robot.Constants.currentLimits;
+import static frc.robot.Constants.digitalIOConstants;
+import static frc.robot.Constants.canId;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
+
+public class IndexerSubsystem extends SubsystemBase {
+
+  enum Mode {
+    INTAKE,
+    EJECT,
+    EJECTPAUSE,
+    REVERSE,
+    STOP
+  };
+
+  private WPI_TalonSRX indexIntake;
+  private WPI_TalonFX indexKicker;
+  private WPI_TalonFX indexBelts;
+
+  private CANSparkMax m_hopperAgitator;
+  private RelativeEncoder m_agitator_encoder;
+  private SparkMaxPIDController agitatorController;
+  private DigitalInput Sensor1 = new DigitalInput(digitalIOConstants.dio0_indexerSensor1);
+  private DigitalInput Sensor2 = new DigitalInput(digitalIOConstants.dio1_indexerSensor2);
+  private DigitalInput Sensor3 = new DigitalInput(digitalIOConstants.dio2_indexerSensor3);
+  private boolean ballReady4IndexerLast = false;
+  private boolean ballExitingLast = false;
+  private boolean ejectBallStep1 = false;
+  private boolean ejectBallStep2 = false;
+  private boolean ejectBallStep3 = false;
+  private int ballCount = 0;
+  private Mode mode = Mode.STOP;
+  // private blinkinSubsystem m_blinkin = RobotContainer.m_blinkin;
+  
+
+  public IndexerSubsystem() {
+
+    indexIntake = new WPI_TalonSRX(canId.canId8_indexo_intake_and_hopper);
+    indexBelts = new WPI_TalonFX(canId.canId10_indexo_belts);
+    indexKicker = new WPI_TalonFX(canId.canId11_indexo_kicker);
+
+    m_hopperAgitator = new CANSparkMax(indexConstants.hopperAgitator, MotorType.kBrushless);
+    m_hopperAgitator.restoreFactoryDefaults();
+    
+    // reduce CAN traffic from Agitator. We don't need fast updates from this motor
+    m_hopperAgitator.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 100);
+    m_hopperAgitator.setPeriodicFramePeriod(PeriodicFrame.kStatus1, 500);
+    m_hopperAgitator.setPeriodicFramePeriod(PeriodicFrame.kStatus2, 500);
+
+    agitatorController = m_hopperAgitator.getPIDController();
+    m_agitator_encoder = m_hopperAgitator.getEncoder();
+    m_hopperAgitator.setInverted(false);
+
+    indexBelts.configFactoryDefault();
+    indexKicker.configFactoryDefault();
+    indexIntake.configFactoryDefault();
+
+    //TODO: Figure out why indexBelts, indexKicker, and indexIntake are causing errors
+    // reduce CAN traffic for motors (not using speed control)
+    /*
+    indexBelts.setStatusFramePeriod(StatusFrame.Status_1_General, 20);
+    indexBelts.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 100);
+    indexKicker.setStatusFramePeriod(StatusFrame.Status_1_General, 20);
+    indexKicker.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 100);
+    indexIntake.setStatusFramePeriod(StatusFrame.Status_1_General, 20);
+    indexIntake.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 100);
+
+    // Voltage limits, percent output is scaled to this new max
+    indexBelts.configVoltageCompSaturation(11);
+    indexBelts.enableVoltageCompensation(true);
+    indexKicker.configVoltageCompSaturation(11);
+    indexKicker.enableVoltageCompensation(true);
+    indexIntake.configVoltageCompSaturation(11);
+    indexIntake.enableVoltageCompensation(true);
+    */
+
+    // current limits
+    indexBelts.configSupplyCurrentLimit(currentLimits.m_currentlimitSecondary);
+
+    // configSupplyCurrentLimit not available on Victors
+    //indexKicker.configSupplyCurrentLimit(currentLimits.m_currentlimitSecondary);
+    //indexIntake.configSupplyCurrentLimit(currentLimits.m_currentlimitSecondary);
+
+    // Brake mode
+    indexBelts.setNeutralMode(NeutralMode.Brake);
+    indexKicker.setNeutralMode(NeutralMode.Brake);
+    indexIntake.setNeutralMode(NeutralMode.Brake);
+
+    // Invert
+    indexBelts.setInverted(false);
+    indexKicker.setInverted(false);
+    indexIntake.setInverted(true);
+
+    indexBelts.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 10);
+    indexKicker.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 10);
+    indexIntake.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+    // TalonFX don't have sensor phase only TalonSRX
+    indexIntake.setSensorPhase(false);
+    indexKicker.setSensorPhase(false);
+    
+    //Set Ramp-Up
+    //indexKicker.configClosedloopRamp(0.1);
+    //indexBelts.configClosedloopRamp(0.1);
+    //indexIntake.configClosedloopRamp(0.1);
+
+    // Config PID values to control RPM
+    indexBelts.config_kP(0, 0.15, 10);
+    indexBelts.config_kI(0, 0.0, 10);
+    indexBelts.config_kD(0, 1.5, 10);
+    indexBelts.config_kF(0, 0.048, 10);
+
+    indexKicker.config_kP(0, 0.15, 10);
+    indexKicker.config_kI(0, 0.0, 10);
+    indexKicker.config_kD(0, 1.5, 10);
+    indexKicker.config_kF(0, 0.053, 10);
+
+    indexIntake.config_kP(0, 0.1, 10);
+    indexIntake.config_kI(0, 0.0, 10);
+    indexIntake.config_kD(0, 0.0, 10);
+    indexIntake.config_kF(0, 0.0, 10);
+
+    agitatorController.setP(0.00003);
+    agitatorController.setI(0.0);
+    agitatorController.setD(0);
+    agitatorController.setFF(0.00012);
+    agitatorController.setIZone(0);
+    agitatorController.setOutputRange(-0.8, 0.8);
+
+  }
+
+  @Override
+  public void periodic() {
+    boolean ballReady4Indexer = ballReadyForIndexer();
+    boolean ballExiting = ballExiting();
+    boolean ballStaged = ballStaged();
+
+    //SmartDashboard.putNumber("Belt Amp", indexerBelts.getcurrent());
+    //SmartDashboard.putNumber("ball count", ballCount);
+    //SmartDashboard.putString("indexer state", mode.name());
+    //SmartDashboard.putNumber("Belt RPM", indexBelts.getSelectedSensorVelocity() * 600 / 2048);
+    //SmartDashboard.putNumber("Kicker RPM", indexKicker.getSelectedSensorVelocity() * 600 / 2048);
+    //SmartDashboard.putNumber("Agitator RPM", m_agitator_encoder.getVelocity());
+
+
+    if (mode == Mode.STOP) {
+      stopIndexer();
+    }
+    if (mode == Mode.EJECT) {
+      setKickerPercentOutput(0.8);
+      setBeltsPercentOutput(0.6);
+      setIntakePercentOutput(0.3);
+    }
+    if (mode == Mode.EJECTPAUSE) {
+      setKickerPercentOutput(0.8);
+      setBeltsPercentOutput(0.0);
+      setHopperPercentOutput(0.0);
+    } 
+    else if (mode == Mode.REVERSE) {
+      setKickerPercentOutput(-0.5);
+      setBeltsPercentOutput(-0.6);
+      setHopperPercentOutput(-0.9);
+    } 
+    else if (mode == Mode.INTAKE) {
+      // Normal, non-eject mode
+      SmartDashboard.putNumber("Eject State", 0);
+
+      if (ballExiting) {
+        // ball exiting, but we're not shooting so stop the belts and kicker
+        stopKicker();
+        stopBelts();
+        if (ballReady4Indexer) {
+          stopHopper();
+        }
+        else {
+          // secondary intake is empty, so keep running the hopper
+          setHopperPercentOutput(0.7);
+        }
+      }
+      else if (ballReady4Indexer == false) {
+        // no ball exiting
+        // no ball staged
+        // no ball in secondary intake, run hopper
+        setHopperPercentOutput(0.7);
+        setAgitatorRPM(Constants.indexConstants.agitatorRPM);
+        setBeltsPercentOutput(0.0);
+        setKickerPercentOutput(0.0);
+      }
+      else {
+        // no ball exiting
+        // no ball staged
+        // ball in secondary intake, pull it into staged
+        setBeltsPercentOutput(0.6);
+        setIntakePercentOutput(0.6);
+      }
+    }
+
+    // increase ball count as balls enter the indexer
+    if (ballReady4Indexer != ballReady4IndexerLast && ballReady4Indexer == false) {
+      ballCount += 1;
+    }
+    ballReady4IndexerLast = ballReady4Indexer;
+
+    // decrease ballCount as balls leave the indexer
+    if (ballExiting != ballExitingLast && ballExiting == false) {
+      ballCount -= 1;
+    }
+    ballExitingLast = ballExiting;
+  }
+
+  public void setBeltsPercentOutput(double percent) {
+    indexBelts.set(ControlMode.PercentOutput, percent);
+  }
+
+  public void setKickerPercentOutput(double percent) {
+    indexKicker.set(ControlMode.PercentOutput, percent);
+  }
+
+  public void setIntakePercentOutput(double percent) {
+      indexIntake.set(ControlMode.PercentOutput, percent);
+  }
+
+  public void setHopperPercentOutput(double percent) {
+    if (percent > 0) {
+      setAgitatorRPM(Constants.indexConstants.agitatorRPM);
+    }
+    else {
+      m_hopperAgitator.set(0.0);
+    }
+    setIntakePercentOutput(percent);
+  }
+
+  public void setBeltsRPM(double rpm) {
+    indexBelts.set(ControlMode.Velocity, rpm * 2048 / 600);
+  }
+
+  public void setKickerRPM(double rpm) {
+    indexKicker.set(ControlMode.Velocity, rpm * 2048 / 600);
+  }
+
+  public void setIntakeRPM(double rpm) {
+    indexIntake.set(ControlMode.Velocity, rpm * 2048 / 600);
+  }
+
+  public void setAgitatorRPM(double rpm){
+    agitatorController.setIAccum(0.0);
+    agitatorController.setReference(rpm, ControlType.kVelocity);
+  }
+
+  public void ejectOneBall() {
+
+    if (mode == Mode.EJECT) {
+      // we're already in ejectMode
+      return;
+    }
+
+    /**
+     * steps:
+     * 1. run indexer until ball exiting (get ready to shoot)
+     * 2. run indexer until ball not exiting (shooting!)
+     * 3. stop indexer when ball ready to exit (ready for next shot)
+     */
+    mode = Mode.EJECT;
+    ejectBallStep1 = true;
+    ejectBallStep2 = ballExiting();
+    ejectBallStep3 = false;
+
+  }
+
+  /**
+   * enable Intake mode, pull balls into intake
+   */
+  public void setEjectPauseMode(){
+    mode = Mode.EJECTPAUSE;
+  }
+
+  /**
+   * enable Intake mode, pull balls into intake
+   */
+  public void setStopMode(){
+    mode = Mode.STOP;
+  }
+
+  /**
+   * enable Intake mode, pull balls into intake
+   */
+  public void setIntakeMode(){
+    mode = Mode.INTAKE;
+  }
+
+  /**
+   * enable Eject mode - eject balls for firing
+   */
+  public void setEjectMode(){
+    mode = Mode.INTAKE;
+  }
+
+  /**
+   * enable Reverse mode - reverse balls out of indexo through hopper
+   */
+  public void setReverseMode(){
+    mode = Mode.REVERSE;
+  }
+
+  /** 
+   * Stop all motors
+   */
+  public void stopIndexer() {
+    mode = Mode.STOP;
+    setBeltsPercentOutput(0.0);
+    setKickerPercentOutput(0.0);
+    setIntakePercentOutput(0.0);
+    setAgitatorRPM(0.0);
+    // m_blinkin.solid_orange();
+  }
+
+  /**
+   * ballReadyForIndexer - monitor sensor 1 for a ball ready to be indexed
+   * 
+   * @return true if a ball is waiting to be indexed
+   */
+  public boolean ballReadyForIndexer() {
+    return ! Sensor1.get();
+  }
+
+  /**
+   * ballStaged - monitor sensor 2 for a ball that is staged
+   * 
+   * @return true if a ball is staged
+   */
+  public boolean ballStaged() {
+    return ! Sensor2.get();
+  }
+
+  /**
+   * ballExiting - monitor sensor 3 for a ball that is at the kickers
+   * 
+   * @return true if a ball is at the kickers
+   */
+  public boolean ballExiting() {
+    return ! Sensor3.get();
+  }
+
+  /**
+   * runIntake() - run intake motor
+   */
+  public void runIntake() {
+    setIntakePercentOutput(1);
+  }
+
+  /**
+   * stopHopper() - stop's intake & Intake Agitator Motors
+   */
+  private void stopHopper() {
+    setHopperPercentOutput(0);
+    setAgitatorRPM(0);
+  }
+  
+  /**
+   * stopBelts() - stop the belts motor
+   */
+  private void stopBelts() {
+    setBeltsRPM(0);
+  }
+
+  /**
+   * stopKicker() - stop the kicker motor
+   */
+  private void stopKicker() {
+    setKickerPercentOutput(0);
+  }
+
+  /**
+   * getBallCount() - return the number of balls in the indexer
+   * 
+   * @return int ball count
+   */
+  public int getBallCount() {
+    return ballCount;
+  }
+
+  /**
+   * setBallCount() - set the number of balls in the indexer
+   */
+  public void setBallCount(int BallCount) {
+    ballCount = BallCount;
+  }
+
+}
