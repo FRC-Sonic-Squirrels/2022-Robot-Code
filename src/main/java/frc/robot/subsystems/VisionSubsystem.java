3fe4aac2ece746176c0ccd0f8817326af2b6c9b1
LuminousLlama <keyushattarde@gmail.com>
2022-01-31 16:54:12 -0800

    fix vision subsystem

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,17 +6,35 @@ import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class VisionSubsystem {
-  PhotonCamera camera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
+public class VisionSubsystem extends SubsystemBase{
+  PhotonCamera m_camera;
+  PhotonPipelineResult m_result;
 
-  PhotonPipelineResult result = camera.getLatestResult();
+  PhotonTrackedTarget m_target;
+  double m_yaw;
+  double m_pitch;
+  double m_area;
+  double m_skew;
+  Transform2d m_pose;
+  List<TargetCorner> m_corners;
 
-  PhotonTrackedTarget target = result.getBestTarget();
-  double yaw = target.getYaw();
-  double pitch = target.getPitch();
-  double area = target.getArea();
-  double skew = target.getSkew();
-  Transform2d pose = target.getCameraToTarget();
-  List<TargetCorner> corners = target.getCorners();
+  public VisionSubsystem(){
+   m_camera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
+  
+  }
+
+  @Override
+  public void periodic() {
+    m_result = m_camera.getLatestResult();
+
+    m_target = m_result.getBestTarget();
+    m_yaw = m_target.getYaw();
+    m_pitch = m_target.getPitch();
+    m_area = m_target.getArea();
+    m_skew = m_target.getSkew();
+    m_pose = m_target.getCameraToTarget();
+    m_corners = m_target.getCorners();
+  }
 }
