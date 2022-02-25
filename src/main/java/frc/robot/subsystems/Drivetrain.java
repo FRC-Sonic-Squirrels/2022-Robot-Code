f45355f6076a7b440cb56bc6062d00e32f277ab2
CodeCreator3 <timellis337@gmail.com>
2022-02-24 19:06:22 -0800

    Polishing

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -365,13 +365,13 @@ public class Drivetrain extends SubsystemBase {
     // Update pose in field simulation
     m_field.setRobotPose(m_odometry.getPoseMeters());
 
-    SmartDashboard.putNumber("Drivetrain IMU yaw", m_pigeon.getYaw());
-    SmartDashboard.putNumber("Drivetrain IMU roll", m_pigeon.getRoll());
-    SmartDashboard.putNumber("Drivetrain IMU temp", m_pigeon.getTemp());
-    SmartDashboard.putNumber("Drivetrain IMU pitch", m_pigeon.getPitch());
-    SmartDashboard.putNumber("Drivetrain IMU compass field strength", m_pigeon.getCompassFieldStrength());
-    SmartDashboard.putNumber("Drivetrain IMU compass heading", m_pigeon.getCompassHeading());
-    SmartDashboard.putNumber("Drivetrain IMU Fused Heading", m_pigeon.getFusedHeading());
-    SmartDashboard.putNumber("Drivetrain IMU absolute compass heading", m_pigeon.getAbsoluteCompassHeading());
+    SmartDashboard.putNumber("Drivetrain IMU Yaw", m_pigeon.getYaw());
+    SmartDashboard.putNumber("Drivetrain IMU Roll", m_pigeon.getRoll());
+    SmartDashboard.putNumber("Drivetrain IMU Temp", m_pigeon.getTemp());
+    SmartDashboard.putNumber("Drivetrain IMU Pitch", m_pigeon.getPitch());
+    SmartDashboard.putNumber("Drivetrain IMU Compass_field_strength", m_pigeon.getCompassFieldStrength());
+    SmartDashboard.putNumber("Drivetrain IMU Compass_heading", m_pigeon.getCompassHeading());
+    SmartDashboard.putNumber("Drivetrain IMU Fused_Heading", m_pigeon.getFusedHeading());
+    SmartDashboard.putNumber("Drivetrain IMU Absolute_compass_heading", m_pigeon.getAbsoluteCompassHeading());
   }
 }
