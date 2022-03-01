b08d9711406047d79299daee021a2cbc41ed1b02
Bryn Dole <bryn.dole@gmail.com>
2022-02-28 17:42:20 -0800

    arm zero command

---
+++ src/main/java/frc/robot/commands/ArmZeroCommand.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class ArmZeroCommand extends CommandBase {
+  private ArmSubsystem arm;
+  private double lastPosition = 999999;  // something really big
+  private int repeatCount = 0;
+
+  public ArmZeroCommand(ArmSubsystem arm) {
+    this.arm = arm;
+    addRequirements(arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    lastPosition = arm.getEncoderValue();
+    arm.setArmPercentOutput(-0.05);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double currentPosition = arm.getEncoderValue();
+    // anything within 5 encoder ticks is close enough to be unchanged
+    if (Math.abs(lastPosition - currentPosition) <= 5) {
+      repeatCount++;
+    }
+    else {
+      repeatCount = 0;
+    }
+    lastPosition = currentPosition;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    arm.setArmPercentOutput(0.0);
+    arm.zeroEncoder();
+  }
+
+  /**
+   * isFinished() - end when we get more than 2 consecutive encoder values that are the same
+   */
+  @Override
+  public boolean isFinished() {
+    return (repeatCount >= 2);
+  }
+}
