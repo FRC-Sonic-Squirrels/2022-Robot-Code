4f62a65e27f7a3e48a943ad2b1be663903947bd4
Bryn Dole <bryn.dole@gmail.com>
2022-03-02 06:10:02 -0800

    toggle climb mode lock/unlock

--- src/main/java/frc/robot/commands/ClimbManualCommand.java
+++ src/main/java/frc/robot/commands/ClimbManualCommand.java
@@ -17,17 +17,18 @@ import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
 public class ClimbManualCommand extends CommandBase {
-  /** Creates a new ClimbManualCommand. */
   ArmSubsystem m_arm;
   ElevatorSubsystem m_elevator;
   XboxController m_controller;
+  boolean m_isUnlocked = false;
 
-  public ClimbManualCommand(ArmSubsystem arm, ElevatorSubsystem elevator, XboxController controller) {
+  public ClimbManualCommand(ArmSubsystem arm, ElevatorSubsystem elevator,
+      XboxController controller) {
     m_arm = arm;
     m_elevator = elevator;
     m_controller = controller;
 
-    //if left bumper pressed again then end command 
+    // if left bumper pressed again then end command
     withInterrupt(() -> m_controller.getLeftBumperPressed());
     addRequirements(arm, elevator);
   }
@@ -42,18 +43,34 @@ public class ClimbManualCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //TODO: check the arm multiplier for changes 
-    double armJoyStickValue = m_controller.getRightY();
-    if(Math.abs(armJoyStickValue) > 0.1){
-      m_arm.setArmPercentOutput(armJoyStickValue * 0.3);
+
+    if (m_controller.getLeftBumperPressed()) {
+      m_isUnlocked = !m_isUnlocked;
+      rumbleSequenceCommand().schedule();
+
+      if (m_isUnlocked == false) {
+        m_arm.setArmPercentOutput(0);
+        m_elevator.stop();
+      }
     }
 
-    double elevatorJoyStickValue = m_controller.getLeftY();
-    if(Math.abs(elevatorJoyStickValue) > 0.1) {
-      m_elevator.brakeOff();
-      m_elevator.setWinchPercentOutput(elevatorJoyStickValue * Constants.ElevatorConstants.elevatorSpeedMultiplier);
-    } else {
-      m_elevator.stop();
+    if (m_isUnlocked) {
+      // TODO: check the arm multiplier for changes
+      double armJoyStickValue = m_controller.getRightY();
+      if (Math.abs(armJoyStickValue) > 0.1) {
+        m_arm.setArmPercentOutput(armJoyStickValue * 0.3);
+      } else {
+        m_arm.setArmPercentOutput(0);
+      }
+
+      double elevatorJoyStickValue = m_controller.getLeftY();
+      if (Math.abs(elevatorJoyStickValue) > 0.1) {
+        m_elevator.brakeOff();
+        m_elevator.setWinchPercentOutput(
+            elevatorJoyStickValue * Constants.ElevatorConstants.elevatorSpeedMultiplier);
+      } else {
+        m_elevator.stop();
+      }
     }
   }
 
