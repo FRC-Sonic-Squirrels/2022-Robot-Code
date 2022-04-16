473826375ec7ade947ec01f50730148c570aa481
Bryn Dole <bryn.dole@gmail.com>
2022-04-16 15:53:07 -0700

    fix merge errors

--- src/main/java/frc/robot/SwerveTrajectoryAutonomousCommandFactory.java
+++ src/main/java/frc/robot/SwerveTrajectoryAutonomousCommandFactory.java
@@ -8,6 +8,7 @@ import java.util.List;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import com.team2930.lib.util.SwerveTestTrajectories;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -16,8 +17,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.drive.Vector2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -47,7 +46,7 @@ public class SwerveTrajectoryAutonomousCommandFactory {
   private static CargoSubsystem m_cargo;
   private static IntakeSubsystem m_intake;
   private static HoodSubsystem m_hood;
-  private static TestTrajectories m_tt;
+  private static SwerveTestTrajectories m_tt;
   private static LimelightSubsystem m_limelight;
   private static Robot m_robot;
 
