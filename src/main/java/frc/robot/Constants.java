0bd9888c079ed6e57d375f4a41231248ab0016d0
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2022-04-21 07:35:16 -0700

    faster rotation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -136,7 +136,7 @@ public final class Constants {
 
   public static class DriveFieldCentricConstant {
     public static final double TRANSLATION_MULTIPLIER = 1.0; 
-    public static final double ROTATION_MULTIPLIER = 0.6; 
+    public static final double ROTATION_MULTIPLIER = 0.7; 
   }
 
   public static final class AutoConstants {
