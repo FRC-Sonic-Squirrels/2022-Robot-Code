4b93b5a7151e62b18cb911f7d823e7ecd37069e0
LuminousLlama <keyushattarde@gmail.com>
2022-09-20 15:50:29 -0700

    Moved old auto climb to new folder for organization

--- src/main/java/frc/robot/commands/AutoClimbCommands/legacy_climb/ClimbHandOff.java
+++ src/main/java/frc/robot/commands/AutoClimbCommands/legacy_climb/ClimbHandOff.java
@@ -2,13 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.AutoClimbCommands;
+package frc.robot.commands.AutoClimbCommands.legacy_climb;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
 import frc.robot.commands.ControllerRumbleCommand;
+import frc.robot.commands.AutoClimbCommands.ArmSetAngle;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
